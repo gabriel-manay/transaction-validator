782c929e4b5af7a31c26cbae1e7fd12ae3025a47
Gabriel Manay <eduardo.g.manay@host.docker.internal>
2021-11-12 10:40:53 -0300

    refactor changes

--- src/main/java/com/accenture/TransactionValidatorApplication.java
+++ src/main/java/com/accenture/TransactionValidatorApplication.java
@@ -1,21 +1,16 @@
 package com.accenture;
 
-import java.util.Map;
 import java.util.function.Function;
 
-import com.accenture.validation.AccountNumberValidator;
-import com.accenture.validation.TransactionCodeQualifierValidator;
-import com.accenture.validation.TransactionComponentSequenceNumberValidator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 
 import com.accenture.entity.Transaction;
-import com.accenture.validation.TransactionCodeValidator;
-import org.springframework.messaging.support.MessageBuilder;
 
 @SpringBootApplication
 public class TransactionValidatorApplication {
@@ -27,44 +22,20 @@ public class TransactionValidatorApplication {
 	}
 
 	@Bean
-	public Function<Message<Map<String,String>>,Message<Transaction>> validate() {
+	public Function<Message<Transaction>, Message<Transaction>> validate() {
 
 		return message -> {
 
-			//Transaction transaction = message.getPayload();
-			/*String content = message.getPayload();
-			String[] data = content.split(",");
-			logger.info(content);
-			Transaction transaction = new Transaction(
-					Integer.parseInt(data[0]),
-					Integer.parseInt(data[1]),
-					Integer.parseInt(data[2]),
-					data[3]
-			);*/
+			Transaction transaction = message.getPayload();
 
-			Map<String,String> content = message.getPayload();
-			Transaction transaction = new Transaction(
-					Integer.parseInt(content.get("transactionCode")),
-					Integer.parseInt(content.get("transactionCodeQualifier")),
-					Integer.parseInt(content.get("transactionComponentSequenceNumber")),
-					content.get("accountNumber")
-			);
-
-			logger.info("Línea recibida: " + transaction);
+			logger.info("Transaccion recibida: " + transaction.toString());
 
 			if (!transaction.isValid()) {
-				logger.info("Transacción errónea: " + transaction);
+				logger.info("Transaccion erronea: " + transaction);
 			}
 			return MessageBuilder.withPayload(transaction).build();
 		};
 
 	}
-
-	/*private void addValidators(Transaction t) {
-		t.addValidator(new TransactionCodeValidator());
-		t.addValidator(new TransactionCodeQualifierValidator());
-		t.addValidator(new TransactionComponentSequenceNumberValidator());
-		t.addValidator(new AccountNumberValidator());
-	}*/
 
 }
